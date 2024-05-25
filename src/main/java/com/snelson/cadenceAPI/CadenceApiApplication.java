81b725fa70eb8af92608e9052dc6b3210bfc3337
Shaun Nelson <shaunptnelson@gmail.com>
2024-05-25 00:06:47 -0400

    Initial commit

---
+++ src/main/java/com/snelson/cadenceAPI/CadenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.snelson.cadenceAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadenceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snelson/cadenceAPI/CadenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snelson.cadenceAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
