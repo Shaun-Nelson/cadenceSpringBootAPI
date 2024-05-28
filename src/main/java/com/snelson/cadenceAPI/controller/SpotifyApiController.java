b6220a32798fc5bb085eedcf45e985c17804710e
Shaun Nelson <shaunptnelson@gmail.com>
2024-05-28 14:45:11 -0400

    Added Spotify authorization functionality

---
+++ src/main/java/com/snelson/cadenceAPI/controller/SpotifyApiController.java
@@ -0,0 +1,68 @@
+package com.snelson.cadenceAPI.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.hc.core5.http.ParseException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.SpotifyHttpManager;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
+
+import java.io.IOException;
+import java.net.URI;
+
+@RestController
+public class SpotifyApiController {
+
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+            .setClientId(System.getenv("CLIENT_ID"))
+            .setClientSecret(System.getenv("CLIENT_SECRET"))
+            .setRedirectUri(SpotifyHttpManager.makeUri(System.getenv("REDIRECT_URI")))
+            .build();
+    private static final String scope = "playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative";
+    private static final String state = generateRandomString(16);
+    private static final String code = "";
+    private static final AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
+            .scope(scope)
+            .state(state)
+            .show_dialog(true)
+            .build();
+    private static final AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
+
+    public static String generateRandomString(int length) {
+        String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder builder = new StringBuilder();
+        while (length-- != 0) {
+            int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
+            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+        }
+        return builder.toString();
+    }
+
+    @GetMapping("/api/login/spotify")
+    public static URI authorizationCodeUri_Sync() {
+        return authorizationCodeUriRequest.execute();
+    }
+
+    @GetMapping("/api/callback")
+    public static void authorizationCode_Sync(HttpServletResponse response) throws IOException {
+        try {
+            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+
+            // Set access and refresh token for further "spotifyApi" object usage
+            spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
+            spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+
+            System.out.println("Expires in: " + authorizationCodeCredentials.getExpiresIn());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        response.sendRedirect(System.getenv("CLIENT_URL"));
+    }
+}
